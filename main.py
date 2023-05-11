1d7e28d2ca02abd4c70cbf724f7991a0c992f130
DennisRojkovski <dennisr2@educbe.ca>
2023-05-11 10:27:34 -0600

    keyboard drawing

--- main.py
+++ main.py
@@ -1,31 +1,57 @@
-'''
-Download zip
-unzip file
-type'cmd' in directory
-type'python main.py' in command prompt
 
-'''
 import pygame
 from pygame import mixer
 import sys
 pygame.init()
 mixer.init()
-display = pygame.display.set_mode((800,800))
+screen = pygame.display.set_mode((800,800))
 
 mixer.music.set_volume(1)
 pygame.mixer.set_num_channels(13)
 
-
-
-'''Hello'''
-
 def play_note(note_name, channel_number, note_file):
   pygame.mixer.Channel(channel_number).play(pygame.mixer.Sound(f'{note_file}'))
+  print(f"Pressed {note_name} Key")
 
 octave = 4
-loop = True
+playing_notes = True
 
-while loop:
+
+
+c1_key = pygame.Rect(4, 600, 96, 300)
+d_key = pygame.Rect(104, 600, 96, 300)
+e_key = pygame.Rect(204, 600, 96, 300)
+f_key = pygame.Rect(304, 600, 96, 300)
+g_key = pygame.Rect(404, 600, 96, 300)
+a_key = pygame.Rect(504, 600, 96, 300)
+b_key = pygame.Rect(604, 600, 96, 300)
+c2_key = pygame.Rect(704, 600, 96, 300)
+
+
+cs_key = pygame.Rect(90, 600, 70, 150)
+ds_key = pygame.Rect(190, 600, 70, 150)
+fs_key = pygame.Rect(390, 600, 70, 150)
+gs_key = pygame.Rect(490, 600, 70, 150)
+as_key = pygame.Rect(590, 600, 70, 150)
+
+
+keys = [c1_key, d_key, e_key, f_key, g_key, a_key, b_key, c2_key, cs_key, ds_key, fs_key, gs_key, as_key,]
+
+
+
+
+
+
+while playing_notes:
+
+  screen.fill((128,128,128))
+  for i in keys:
+    if i != cs_key and i != ds_key and i != fs_key and i != gs_key and i != as_key:
+      pygame.draw.rect(screen, (255,255,255), i)
+    else:
+      pygame.draw.rect(screen, (0,0,0), i)
+  pygame.display.flip()
+
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       pygame.quit()
